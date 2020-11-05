2b4f147b1e4db263726bf9421ec71bab5c5be988
Rich Chau <richsensations@gmail.com>
2020-11-05 08:54:30 -0800

    new heroku cleardb config

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '55f7fa8fbe8cbb2e27255c9faa7d8ed9064c5405',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '55f7fa8fbe8cbb2e27255c9faa7d8ed9064c5405',
+    ),
+  ),
+);
