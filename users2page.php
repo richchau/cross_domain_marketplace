931b06ed4ad4bdfc2c329bdf5bec9e2620c937d7
Rich Chau <richsensations@gmail.com>
2020-11-05 09:02:43 -0800

    update cleardb php

--- users2page.php
+++ users2page.php
@@ -10,7 +10,13 @@
 <body>
     <?php
 
-    $conn = mysqli_connect("localhost", "root", "root", "usersdb");
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    $conn = mysqli_connect($server, $username, $password, $db);
 
     if ($conn->connect_error) {
         die("Connection failed:" . $conn->connect_error);
