5098099771e21b986c5005d42f0f966aa88c22ea
Rich Chau <richsensations@gmail.com>
2020-11-05 15:56:51 -0800

    json encode users

---
+++ allUsersPage.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    function get_data($url)
+    {
+        $ch = curl_init();
+        $timeout = 5;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+
+    $returned_content = get_data('http://potato-katie.com/users2.php');
+
+    print($returned_content[0]);
+    ?>
+</body>
+
+</html>
--- users2page.php
+++ users2page.php
@@ -10,14 +10,16 @@
 <body>
     <?php
 
+    /*
     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    print($url);
-    $server = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $db = substr($url["path"], 1);
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $db = substr($url["path"], 1);
 
-    $conn = mysqli_connect($server, $username, $password, $db);
+    */
+
+    $conn = mysqli_connect("localhost", "root", "root", "usersdb");
 
     if ($conn->connect_error) {
         die("Connection failed:" . $conn->connect_error);
@@ -25,34 +27,42 @@
 
 
     $query = "SELECT * FROM users";
-    if ($result = mysqli_query($conn, $query)) {
-        echo "<table>";
-        echo "<tr>";
-        echo "<th>id</th>";
-        echo "<th>first_name</th>";
-        echo "<th>last_name</th>";
-        echo "<th>email</th>";
-        echo "<th>address</th>";
-        echo "<th>home_phone</th>";
-        echo "<th>cell_phone</th>";
-        echo "</tr>";
-        while ($row = mysqli_fetch_array($result)) {
-            echo "<tr>";
-            echo "<td>" . $row['id'] . "</td>";
-            echo "<td>" . $row['first_name'] . "</td>";
-            echo "<td>" . $row['last_name'] . "</td>";
-            echo "<td>" . $row['email'] . "</td>";
-            echo "<td>" . $row['address'] . "</td>";
-            echo "<td>" . $row['home_phone'] . "</td>";
-            echo "<td>" . $row['cell_phone'] . "</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-        // Free result set
-        mysqli_free_result($result);
-    } else {
-        echo "ERROR: Could not able to execute $query. " . mysqli_error($conn);
+    $result = $conn->query($query);
+    $rows = array();
+
+    while ($r = mysqli_fetch_assoc($result)) {
+        $rows[] = $r;
     }
+
+    print(json_encode($rows));
+    // if ($result = mysqli_query($conn, $query)) {
+    //     echo "<table>";
+    //     echo "<tr>";
+    //     echo "<th>id</th>";
+    //     echo "<th>first_name</th>";
+    //     echo "<th>last_name</th>";
+    //     echo "<th>email</th>";
+    //     echo "<th>address</th>";
+    //     echo "<th>home_phone</th>";
+    //     echo "<th>cell_phone</th>";
+    //     echo "</tr>";
+    //     while ($row = mysqli_fetch_array($result)) {
+    //         echo "<tr>";
+    //         echo "<td>" . $row['id'] . "</td>";
+    //         echo "<td>" . $row['first_name'] . "</td>";
+    //         echo "<td>" . $row['last_name'] . "</td>";
+    //         echo "<td>" . $row['email'] . "</td>";
+    //         echo "<td>" . $row['address'] . "</td>";
+    //         echo "<td>" . $row['home_phone'] . "</td>";
+    //         echo "<td>" . $row['cell_phone'] . "</td>";
+    //         echo "</tr>";
+    //     }
+    //     echo "</table>";
+    //     // Free result set
+    //     mysqli_free_result($result);
+    // } else {
+    //     echo "ERROR: Could not able to execute $query. " . mysqli_error($conn);
+    // }
 
     $conn->close();
     ?>
