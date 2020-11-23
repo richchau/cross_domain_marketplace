c12c3c3a075d5327385326052b151a90f9f8674c
Rich Chau <richsensations@gmail.com>
2020-11-22 23:31:46 -0800

    cURL configs

--- cURLs/top_rated_products.php
+++ cURLs/top_rated_products.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
     <?php
     include("curlSetUp.php");
     $query = 'SELECT name, image, product_id, pricing, product_rating FROM products ORDER BY product_rating DESC limit 5';
     getCURLdata($query);
     ?>
-</body>
-
-</html>
--- dbconfig.php
+++ dbconfig.php
@@ -7,7 +7,7 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $conn = mysqli_connect($server, $username, $password, $db);
-//$conn = mysqli_connect("localhost", "root", "root", "oxy");
+// $conn = mysqli_connect("localhost", "root", "root", "oxy");
 
 if ($conn->connect_error) {
     die("Connection failed:" . $conn->connect_error);
