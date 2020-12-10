1206a0192a951f077d65f68f109cf0d1c36b3e52
Rich Chau <richsensations@gmail.com>
2020-12-09 17:04:48 -0800

    logout function fixes

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -1,5 +1,5 @@
 <?php
 unset($_COOKIE['name']);
 setcookie('name', null, -1, '/');
-header('location: ../index.php');
+header('location: http://potato-katie.com/marketplace.php?logout=true ');
 exit();
